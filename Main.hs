d37d0fd3785e2e7e78249fe9553775de56f6b5d3
Carlo Emil Angiuli <carlo@carloangiuli.com>
2012-05-22 21:24:35 -0400

    Now takes input as a filename argument.

--- Main.hs
+++ Main.hs
@@ -1,13 +1,26 @@
+-- latex2wp
+-- Carlo Angiuli
+
 module Main where
 
 import Text.Pandoc
 import System.Process
 import System.IO
+import System.Environment
 
 main = do
-  latex <- getContents
+  args <- getArgs
+  if null args then putStr usage else do
+  latex <- readFile $ head args
   pandoc <- bottomUpM wpLatex $ readDoc latex
   putStrLn $ writeDoc pandoc
+
+usage = unlines
+  ["Usage: latex2wp filename",
+   "",
+   "Convert LaTeX input files to HTML source suitable for WordPress.",
+   "Uses WP-LaTeX syntax ($latex $) for inline math,",
+   "and converts display math to PNG."]
 
 -- Going from/to Pandoc
 -- Translates $x$ to <span class="LaTeX">$x$</span>
@@ -24,11 +37,10 @@ wpLatex e = case e of
   Math DisplayMath x -> displayMath x
   _ -> return e
 
--- render display math with latex2png script
--- TODO alt/title text
+-- render display math with math2png script
 displayMath x = do
   (stdin,stdout,_,_) <- runInteractiveCommand "./math2png"
   hPutStr stdin x
   filename <- hGetContents stdout
-  return $ Image [] (filename,"")
+  return $ Image [] (filename,x)
 
